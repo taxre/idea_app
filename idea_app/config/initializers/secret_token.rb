e63b8f274a83d5439e3eaf0f998f20a23897f078
Valeri <valerichakarov@yahoo.com>
2013-11-09 11:02:07 +0000

    Initialize repository

---
+++ idea_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeaApp::Application.config.secret_key_base = '85eb84b59e297c04d843f54a7dcb96a95a0a1536d9cd7b2d239bda404486264d941b2ab8e14a013bf7357c805c30594ad119d0677c21e59cda172442b4f4676f'
